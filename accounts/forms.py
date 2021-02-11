add30901644994e5363efa545d01c23576fd4b8a
Edward Boland <mr.eboland@gmail.com>
2021-02-11 17:38:19 -0500

    added code to make email mandatory

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    # Testing to see how to make email mandatory
+    # Pulled from:
+    # https://stackoverflow.com/questions/7682804/django-model-forms-setting-a-required-field
+    email = forms.EmailField(required=True)
     # We use the Meta class to override the default fields by setting the model to our CustomUser and using the default fields view Meta.fields which includes all default fields. To add our custom age field we simply tack it on at the end and it will display automatically on our future sign up page.
     class Meta(UserCreationForm):
         # Our CustomUser model contains all the fields of the default (username, first_name, last_name, email, password, groups, and more) User model and our additional age field which we set. However the default fields on UserCreationForm is just username, email and password which doesn't include everythin. We will expand on it later.
