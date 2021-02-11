417eb81f656abae8180b355e33fba545d2a9b2ac
Edward Boland <mr.eboland@gmail.com>
2021-02-10 22:20:11 -0500

    added basic urls and views for our pages app

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Using Django's TemplateView generic class-based view
+class HomePageView(TemplateView):
+    template_name = "home.html"
