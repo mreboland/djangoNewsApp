dcf5ef4a0a886b3a266fbe3e97d76f19b20a597e
Edward Boland <mr.eboland@gmail.com>
2021-02-11 19:17:21 -0500

    added urls and views for viewing, updating, and modifying our articles. Added html files to facilitate

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleUpdateView,
+    ArticleDetailView,
+    ArticleDeleteView,
+)
 
+# Django automatically adds a primary key to each
+# database. Therefore our first article with a primary key of 1 will be at articles/1/edit / and the
+# delete route will be at articles/1/delete/.
 urlpatterns = [
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     path("", ArticleListView.as_view(), name="article_list"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,31 @@
-from django.views.generic import ListView
+# List view is to show many items (a list), detail view is used to show 1 item.
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Article
 
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+    
+
+# will use Django’s generic class-based views for DetailView, UpdateView and DeleteView. We specify
+# which fields can be updated–title and body–and where to redirect the user after deleting an
+# article: article_list.
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+    
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = (
+        "title",
+        "body",
+    )
+    template_name = "article_edit.html"
+    
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
