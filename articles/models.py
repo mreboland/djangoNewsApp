25ea744d0942eaaa3688ab554d602c59c09e10fd
Edward Boland <mr.eboland@gmail.com>
2021-02-11 19:00:36 -0500

    added urls, views, and templates for our articles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+# We want to use our custom user model which we set in config/settings.py as AUTH_USER_MODEL. We can do so by importing get_user_model. We use said model for our author, the user.
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    # Allows us to view the model in our admin interface
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("article_detail", args=[str(self.id)])
